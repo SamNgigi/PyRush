16ab5156e90815e5b7fbb09142dd4e82588bca6d
SamNgigi <sam.t.ngigi@gmail.com>
2025-04-25 14:10:01 +0300

    [feat]: Adds graph challenge can_finish_course_schedule

--- src/data_structures_algorithms/graphs/graph_challenges.py
+++ src/data_structures_algorithms/graphs/graph_challenges.py
@@ -279,6 +279,7 @@ def copy_graph_med(node: Optional["LeetNode"]) -> Optional["LeetNode"]:
         return None
     copy_map_dfs = {}
     copy_map_bfs = {}
+
     def bfs(node: "LeetNode") -> "LeetNode":
         
         copy_map_bfs[node] = LeetNode(node.val)
@@ -314,7 +315,61 @@ def copy_graph_med(node: Optional["LeetNode"]) -> Optional["LeetNode"]:
 
     return bfs_result
 
+def can_finish_course_schedule(numCourses: int, prerequisites: List[List[int]]) -> bool:
+    """
+    There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
 
+    For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+    Return true if you can finish all courses. Otherwise, return false.
+
+     
+
+    Example 1:
+
+    Input: numCourses = 2, prerequisites = [[1,0]]
+    Output: true
+    Explanation: There are a total of 2 courses to take. 
+    To take course 1 you should have finished course 0. So it is possible.
+    Example 2:
+
+    Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+    Output: false
+    Explanation: There are a total of 2 courses to take. 
+    To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+     
+
+    Constraints:
+
+    1 <= numCourses <= 2000
+    0 <= prerequisites.length <= 5000
+    prerequisites[i].length == 2
+    0 <= ai, bi < numCourses
+    All the pairs prerequisites[i] are unique.
+    """
+    course_preReq_graph = {i:[] for i in range(numCourses)}
+    
+    for crs, preReq in prerequisites:
+        course_preReq_graph[crs].append(preReq)
+
+    visited = set()
+
+    def dfs(crs):
+        if crs in visited:
+            return False
+        if course_preReq_graph[crs] == []:
+            return True
+        
+        visited.add(crs)
+        for preReq in course_preReq_graph[crs]:
+            if not dfs(preReq): return False
+        visited.remove(crs)
+        course_preReq_graph[crs] = []
+        return True
+    
+    for i in range(numCourses):
+        if not dfs(i): return False
+    
+    return True
 
 
 if __name__ == "__main__":
