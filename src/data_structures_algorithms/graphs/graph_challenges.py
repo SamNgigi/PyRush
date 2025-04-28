906c79b692d0cd4238486d0a068e280281f9a66d
SamNgigi <sam.t.ngigi@gmail.com>
2025-04-28 21:28:39 +0300

    [feat]: Adds graph rotten_oranges_med challenge

--- src/data_structures_algorithms/graphs/graph_challenges.py
+++ src/data_structures_algorithms/graphs/graph_challenges.py
@@ -371,6 +371,76 @@ def can_finish_course_schedule(numCourses: int, prerequisites: List[List[int]]) 
     
     return True
 
+
+def rotten_oranges_med(grid: List[List[int]]) -> int:
+    """
+    You are given an m x n grid where each cell can have one of three values:
+
+    0 representing an empty cell,
+    1 representing a fresh orange, or
+    2 representing a rotten orange.
+    Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
+
+    Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
+
+     
+
+    Example 1:
+
+
+    Input: grid = [[2,1,1],[1,1,0],[0,1,1]]
+    Output: 4
+    Example 2:
+
+    Input: grid = [[2,1,1],[0,1,1],[1,0,1]]
+    Output: -1
+    Explanation: The orange in the bottom left corner (row 2, column 0) is never rotten, because rotting only happens 4-directionally.
+    Example 3:
+
+    Input: grid = [[0,2]]
+    Output: 0
+    Explanation: Since there are already no fresh oranges at minute 0, the answer is just 0.
+     
+
+    Constraints:
+
+    m == grid.length
+    n == grid[i].length
+    1 <= m, n <= 10
+    grid[i][j] is 0, 1, or 2.
+    """
+    EMPTY, FRESH, ROTTEN = 0, 1, 2
+    ROWS, COLS = len(grid), len(grid[0])
+    neighbor_coords = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    fresh_count = 0
+    queue = deque()
+
+    for row in range(ROWS):
+        for col in range(COLS):
+            if grid[row][col] == ROTTEN: # Our bfs starts from rotten oranges
+                queue.append((row, col))
+            if grid[row][col] == FRESH:
+                fresh_count += 1
+    if fresh_count == 0: return 0
+    if not queue: return -1
+
+    min_minutes = 0
+
+    # Multi-source BFS
+    while queue and fresh_count > 0:
+        for _ in range(len(queue)):
+            cr, cc = queue.popleft()
+            for ncr, ncc in neighbor_coords:
+                nr, nc = ncr + cr, ncc + cc
+                if(0 <= nr < ROWS and 0 <= nc < COLS and grid[nr][nc] == 1):
+                    grid[nr][nc] = ROTTEN
+                    fresh_count -= 1
+                    queue.append((nr, nc))
+        min_minutes += 1
+
+    return min_minutes if fresh_count == 0 else -1
+
+
 def shortest_path_to_get_food_med():
     """
     You are starving and you want to eat food as quickly as possible. You want to find the shortest path to arrive at any food cell.
