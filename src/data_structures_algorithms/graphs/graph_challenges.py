5527ca443e3c6bee537a81cb939e17124e089ee2
SamNgigi <sam.t.ngigi@gmail.com>
2025-05-02 21:30:32 +0300

    [feat]: Adds get_max_gold to graph_challenges

--- src/data_structures_algorithms/graphs/graph_challenges.py
+++ src/data_structures_algorithms/graphs/graph_challenges.py
@@ -405,6 +405,90 @@ def rotten_oranges_med(grid: List[List[int]]) -> int:
     return min_minutes if fresh_count == 0 else -1
 
 
+def path_with_max_gold_med(grid: List[List[int]]) -> int:
+    """
+    In a gold mine grid of size m x n, each cell in this mine has an integer representing the amount of gold in that cell, 0 if it is empty.
+
+    Return the maximum amount of gold you can collect under the conditions:
+
+    Every time you are located in a cell you will collect all the gold in that cell.
+    From your position, you can walk one step to the left, right, up, or down.
+    You can't visit the same cell more than once.
+    Never visit a cell with 0 gold.
+    You can start and stop collecting gold from any position in the grid that has some gold.
+     
+
+    Example 1:
+
+    Input: grid = [[0,6,0],[5,8,7],[0,9,0]]
+    Output: 24
+    Explanation:
+    [[0,6,0],
+     [5,8,7],
+     [0,9,0]]
+    Path to get the maximum gold, 9 -> 8 -> 7.
+    Example 2:
+
+    Input: grid = [[1,0,7],[2,0,6],[3,4,5],[0,3,0],[9,0,20]]
+    Output: 28
+    Explanation:
+    [[1,0,7],
+     [2,0,6],
+     [3,4,5],
+     [0,3,0],
+     [9,0,20]]
+    Path to get the maximum gold, 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7.
+     
+
+    Constraints:
+
+    m == grid.length
+    n == grid[i].length
+    1 <= m, n <= 15
+    0 <= grid[i][j] <= 100
+    There are at most 25 cells containing gold.
+    """
+    
+    ROWS, COLS = len(grid), len(grid[0])
+    neighbors = ((-1, 0), (1, 0), (0, -1), (0, 1))
+    max_gold = 0
+
+    # Used [] for list here as opposed to () for generator which only allows for one pass iteration
+    gold_cells = [(r, c) for r in range(ROWS) for c in range(COLS) if grid[r][c] > 0]
+    gold_total = sum(grid[r][c] for r, c in gold_cells)
+
+    def dfs(row: int, col: int) -> int:
+        if(
+            min(row, col) < 0 or
+            row >= ROWS or col >= COLS or
+            grid[row][col] == 0
+        ):
+            return 0
+        gold_here = grid[row][col]
+        grid[row][col] = 0 # visited
+        current_max = 0
+
+        for r, c in neighbors:
+            nr, nc = row + r, col + c
+            if(
+                0 <= nr < ROWS and
+                0 <= nc < COLS and
+                grid[nr][nc] != 0
+            ):
+                current_max = max(current_max, dfs(nr, nc))
+
+        grid[row][col] = gold_here
+        return gold_here + current_max
+
+
+    for r, c in gold_cells:
+        max_gold = max(max_gold, dfs(r, c))
+        if max_gold == gold_total:
+            return gold_total
+
+    return max_gold
+
+
 def shortest_path_to_get_food_med():
     """
     You are starving and you want to eat food as quickly as possible. You want to find the shortest path to arrive at any food cell.
