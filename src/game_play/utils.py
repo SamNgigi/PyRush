10c1c683bf00d3dbd99691e7753f88f3d7931759
SamNgigi <sam.t.ngigi@gmail.com>
2025-05-06 14:57:07 +0300

    [feat]: Adds topological_sort stub

--- src/game_play/utils.py
+++ src/game_play/utils.py
@@ -24,36 +24,24 @@ if __name__ == "__main__":
 
     html_snippet = """
 <div class="html">
-<p>Implement a <span>MinHeap</span> class that supports:</p>
-<ul>
-  <li>Building a Min Heap from an input array of integers.</li>
-  <li>Inserting integers in the heap.</li>
-  <li>Removing the heap's minimum / root value.</li>
-  <li>Peeking at the heap's minimum / root value.</li>
-  <li>
-    Sifting integers up and down the heap, which is to be used when inserting
-    and removing values.
-  </li>
-</ul>
-<p>Note that the heap should be represented in the form of an array.</p>
 <p>
-  If you're unfamiliar with Min Heaps, we recommend watching the
-  Conceptual Overview section of this question's video explanation before
-  starting to code.
+  You're given a list of arbitrary jobs that need to be completed; these jobs
+  are represented by distinct integers. You're also given a list of dependencies. A
+  dependency is represented as a pair of jobs where the first job is a
+  prerequisite of the second one. In other words, the second job depends on the
+  first one; it can only be completed once the first job is completed.
 </p>
-<h3>Sample Usage</h3>
-<pre><span class="CodeEditor-promptParameter">array</span> = [48, 12, 24, 7, 8, -5, 24, 391, 24, 56, 2, 6, 8, 41]
-
-<span class="CodeEditor-promptComment">// All operations below are performed sequentially.</span>
-<span class="CodeEditor-promptParameter">MinHeap</span>(array): - <span class="CodeEditor-promptComment">// instantiate a MinHeap (calls the buildHeap method and populates the heap)</span>
-<span class="CodeEditor-promptParameter">buildHeap</span>(array): - <span class="CodeEditor-promptComment">[-5, 2, 6, 7, 8, 8, 24, 391, 24, 56, 12, 24, 48, 41]</span>
-<span class="CodeEditor-promptParameter">insert</span>(76): - <span class="CodeEditor-promptComment">[-5, 2, 6, 7, 8, 8, 24, 391, 24, 56, 12, 24, 48, 41, 76]</span>
-<span class="CodeEditor-promptParameter">peek</span>(): -5
-<span class="CodeEditor-promptParameter">remove</span>(): -5 <span class="CodeEditor-promptComment">[2, 7, 6, 24, 8, 8, 24, 391, 76, 56, 12, 24, 48, 41]</span>
-<span class="CodeEditor-promptParameter">peek</span>(): 2
-<span class="CodeEditor-promptParameter">remove</span>(): 2 <span class="CodeEditor-promptComment">[6, 7, 8, 24, 8, 24, 24, 391, 76, 56, 12, 41, 48]</span>
-<span class="CodeEditor-promptParameter">peek</span>(): 6
-<span class="CodeEditor-promptParameter">insert</span>(87): - <span class="CodeEditor-promptComment">[6, 7, 8, 24, 8, 24, 24, 391, 76, 56, 12, 41, 48, 87]</span>
+<p>
+  Write a function that takes in a list of jobs and a list of dependencies and
+  returns a list containing a valid order in which the given jobs can be
+  completed. If no such order exists, the function should return an empty array.
+</p>
+<h3>Sample Input</h3>
+<pre><span class="CodeEditor-promptParameter">jobs</span> = [1, 2, 3, 4]
+<span class="CodeEditor-promptParameter">deps</span> = [[1, 2], [1, 3], [3, 2], [4, 2], [4, 3]]
+</pre>
+<h3>Sample Output</h3>
+<pre>[1, 4, 3, 2] or [4, 1, 3, 2]
 </pre>
 </div>
 """
